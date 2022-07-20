37c5fffb0114287004724e31757fed5607274f20
Nishant <nishanttechpro5@gmail.com>
2022-07-20 17:58:46 +0530

    addSubscription

---
+++ BeMyHealth_WebApi/ContextData/BeMyHealthDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BeMyHealth_WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeMyHealth_WebApi.ContextData
+{
+    public class BeMyHealthDbContext : DbContext
+    {
+        public BeMyHealthDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CustomSubscription> CustomSubscriptions { get; set; }
+        public DbSet<CustomDietPlan> CustomDietPlans { get; set; }
+     }
+}
