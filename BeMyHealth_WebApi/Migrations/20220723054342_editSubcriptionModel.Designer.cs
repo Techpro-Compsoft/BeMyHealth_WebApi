dabcf588815f46be41458d0ff31909edfef38d12
Nishant <nishanttechpro5@gmail.com>
2022-07-23 11:14:17 +0530

    fixed

--- BeMyHealth_WebApi/Migrations/20220723054342_editSubcriptionModel.Designer.cs
+++ BeMyHealth_WebApi/Migrations/20220723054342_editSubcriptionModel.Designer.cs
@@ -4,6 +4,7 @@ using BeMyHealth_WebApi.ContextData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BeMyHealth_WebApi.Migrations
 {
     [DbContext(typeof(BeMyHealthDbContext))]
-    partial class BeMyHealthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220723054342_editSubcriptionModel")]
+    partial class editSubcriptionModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BeMyHealth_WebApi/Migrations/20220723054342_editSubcriptionModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeMyHealth_WebApi.Migrations
+{
+    public partial class editSubcriptionModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Duration",
+                table: "CustomSubscriptions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "CustomSubscriptions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "CustomSubscriptions");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Duration",
+                table: "CustomSubscriptions",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
